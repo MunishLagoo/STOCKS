ada50a1b505900507f89ac337c936d0070192234
Munish Lagoo <munishlagoo@gmail.com>
2021-11-01 15:38:20 -0400

    Commit Stocks to branch

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { StocksService } from './services/stocks.service';
+
+import { AppRoutes } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryComponent,
+    DashboardComponent,
+    ManageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutes,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [StocksService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
